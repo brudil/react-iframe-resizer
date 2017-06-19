79332fe8c672da5eb66fe19a6bcfdaaacccf1fe7
David J. Bradshaw <dave@bradshaw.net>
2017-06-19 13:32:31 +0100

    Add onEvent handlers

--- src/index.js
+++ src/index.js
@@ -4,32 +4,64 @@ import omit from 'lodash.omit';
 import objectAssign from 'object-assign';
 import iframeResizer from 'iframe-resizer/js/iframeResizer';
 
-class ResizerManagedIframe extends React.Component {
+function noop() {}
+
+class ResizerManagedIframe extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.callbacks = {
+      closeCallback: props.onCloseRequest || noop,
+      initCallback: props.onInit || noop,
+      messageCallback: props.onMessage || noop,
+      resizeCallback: props.onResize || noop,
+      scrollCallback: props.onScroll || noop,
+    };
+
+    this.omitProps = [
+      'children',
+      'options',
+      'onCloseRequest',
+      'onInit',
+      'onMessage',
+      'onResize',
+      'onScroll',
+    ];
+
+    this.ref = ref => this.iframeElement = ref;
+  }
+
   componentDidMount() {
-    this.iframeResizer = iframeResizer(this.props.options, this.iframeElement);
+    const options = Object.assign({ __mode: 'react' }, this.props.options, this.callbacks);
+    this.iframeResizer = iframeResizer(options, this.iframeElement);
   }
 
   componentWillUnmount() {
     this.iframeResizer.close();
+    delete this.iframeResizer;
   }
 
   render() {
     return (
       <iframe
-        {...omit(this.props, ['children', 'options'])}
-        ref={ref => {
-          this.iframeElement = ref;
-        }}
-      >{this.children}</iframe>
+        {...omit(this.props, this.omitProps)}
+        ref={this.ref}
+      >
+        { this.children }
+      </iframe>
     );
   }
 }
 
 ResizerManagedIframe.propTypes = {
-  id: React.PropTypes.string.isRequired,
   children: React.PropTypes.node,
   src: React.PropTypes.string,
   options: React.PropTypes.object,
+  onCloseRequest: React.PropTypes.func,
+  onInit: React.PropTypes.func,
+  onMessage: React.PropTypes.func,
+  onResize: React.PropTypes.func,
+  onScroll: React.PropTypes.func,
 };
 
 ResizerManagedIframe.defaultProps = {
